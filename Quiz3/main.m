370984a560d1853521c5ac41bd88fe041acb02f5
Wes Moser <Moser.Wesley@gmail.com>
2014-01-31 14:35:14 -0500

    Initial Commit

---
+++ Quiz3/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Quiz3
+//
+//  Created by Moser, Wesley on 1/31/14.
+//  Copyright (c) 2014 Moser, Wesley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
